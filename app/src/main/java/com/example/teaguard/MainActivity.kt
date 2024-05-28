bbf6b05944561beb11ac90eca0197451cf1d66a6
wisnu andika <wisnuandika104@gmail.com>
2024-05-28 22:07:56 +0700

    feat: add 2 button in home screen and primary color to green

--- app/src/main/java/com/example/teaguard/MainActivity.kt
+++ app/src/main/java/com/example/teaguard/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.teaguard
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
